fdd941f4493c88a8fc67b9f5aca819c2e13c31ad
jerinraj5555 <jerinraj5555@gmail.com>
2022-09-04 16:34:34 +0530

    fc

---
+++ src/main/java/org/example/usage/Main.java
@@ -0,0 +1,8 @@
+package org.example.usage;
+
+public class Main {
+    public static void main(String[] args) {
+        org.example.Main.display("It's a usage class !!!!!!!!!!!!!!!!!");
+        System.out.println("Hello world!");
+    }
+}
